330bba2080c2a3fc20c7d956609b93384aced9c6
facu <facundo.moschion@gmail.com>
2025-12-05 20:33:34 +0100

    agregados de trabajos

---
+++ studio-java/01-tipos/01-variables.js
@@ -0,0 +1,25 @@
+
+/* Una varible no debe iniciar con numeros yno se puede usar una palbra reservada
+como let let = "hola mundo"
+cuando se crean variables se pueden seguir 3 convenciones 
+1 UpperCamelCase: esta convencion hace que cada vez que escribamos una palabra en 
+nuestra variable la separemos usando mayusculas ej let NombreApellido = "..."
+2 camelCase: camelCase nos dice que podemos usar mayusculas para separar pero en 
+la primera palabra no es necesario por ej let nombreApellido = "...".Esta es la más utilizada
+3 snake_case: en cambio esta nos indica que cada vez que separemos las palabras usaremos 
+el guion bajo por ej nombre_apellido = "..."*/
+
+//variable
+let nombre = 'Hola Mundo';
+let NombreApellido;
+let nombreApellido;
+let nombre_pellido;
+
+console.log(nombre);
+
+let apellido;  //en este caso las variables estan nombradas pero todavia no tienen un valor
+let animal; //para darle un valor ya no es necesario usar el let nuevamente
+apellido = 'Moschion' //aca le di el valor sin usar el let
+
+//Tipos de datos
+
